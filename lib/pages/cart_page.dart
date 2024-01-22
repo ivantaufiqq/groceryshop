0931151a25f479d02adad59c53d921e97dda253e
ivantaufiqq <itaufiq11@gmail.com>
2024-01-22 16:44:14 +0700

    add function remove item from cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:groceryshop/model/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -7,6 +9,45 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Cart')),
+      body: Consumer<CartModel>(
+        builder: (context, value, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  padding: EdgeInsets.all(12),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 36,
+                          ),
+                          title: Text(value.cartItems[index][0]),
+                          subtitle: Text('Rp' + value.cartItems[index][1]),
+                          trailing: IconButton(
+                            icon: Icon(Icons.cancel),
+                            onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                    .removeItemFromCart(index),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
